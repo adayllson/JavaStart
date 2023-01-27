e6791fa8d50a3033b80ee8f6789647da4878ed1e
adailson-almeida <adayllson.almeida@gmail.com>
2023-01-27 15:17:41 -0300

    estrutura switch case

---
+++ 5-EstruturaSwitchCase/app/src/main/java/estruturaswitchcase/App.java
@@ -0,0 +1,24 @@
+
+package estruturaswitchcase;
+
+public class App {
+    
+    public static void main(String[] args) {
+       
+        int codigoProduto = 2;
+
+        switch (codigoProduto) {
+
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            default:     
+        }
+        
+    }
+}
