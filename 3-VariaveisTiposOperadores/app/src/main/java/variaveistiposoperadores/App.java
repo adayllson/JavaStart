70c38e5905581950af9251a955bb1f7f62635040
adailson-almeida <adayllson.almeida@gmail.com>
2023-01-27 10:11:40 -0300

    tipos de variáveis

--- 3-VariaveisTiposOperadores/app/src/main/java/variaveistiposoperadores/App.java
+++ 3-VariaveisTiposOperadores/app/src/main/java/variaveistiposoperadores/App.java
@@ -4,6 +4,12 @@ public class App {
     
     public static void main(String[] args) {
         
-        
+        int idade = 0;
+        float cotacaoDolar = 5.0f;
+        double cotacaoEuro = 6.0d;
+        char genero = 'M';
+        byte placar = 0;
+        boolean estaCadastrado = false;
+        String nome = "Texto da variável.";
     }
 }
