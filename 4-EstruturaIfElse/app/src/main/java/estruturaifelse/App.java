27cd87c480280af4ac998c7a13dd5f0c7a924fff
adailson-almeida <adayllson.almeida@gmail.com>
2023-01-27 11:24:38 -0300

    estrutura if else

---
+++ 4-EstruturaIfElse/app/src/main/java/estruturaifelse/App.java
@@ -0,0 +1,20 @@
+
+package estruturaifelse;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        int media = 7;
+
+        if (media >= 7) {
+            if (media == 10) {
+
+            } else {
+
+            }
+        } else {
+
+        }
+    }
+}
