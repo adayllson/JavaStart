6675483f994890c3d1a287601c176d577b3254e6
adailson-almeida <adayllson.almeida@gmail.com>
2023-02-01 18:15:22 -0300

    classes

---
+++ 11 - Classes/app/src/main/java/classes/App.java
@@ -0,0 +1,12 @@
+
+package classes;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
