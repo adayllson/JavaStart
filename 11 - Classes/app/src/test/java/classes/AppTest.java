60cd760421407efc8a67f0ede15d28f20ef159f7
adailson-almeida <adayllson.almeida@gmail.com>
2023-02-03 17:51:20 -0300

    classes

--- 11 - Classes/app/src/test/java/classes/AppTest.java
+++ 11 - Classes/app/src/test/java/classes/AppTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        Pessoa classUnderTest = new Pessoa();
+        assertNotNull("app should have a greeting");
     }
 }
