d8de5b3f92d95a7b1d29de6f3be10dfa900a7a49
adailson-almeida <adayllson.almeida@gmail.com>
2023-02-15 10:41:37 -0300

    exercicio 20

--- 13 - ExercicioSintaxe/app/src/main/java/exerciciosintaxe/exerciciosintaxe/App.java
+++ 13 - ExercicioSintaxe/app/src/main/java/exerciciosintaxe/exerciciosintaxe/App.java
@@ -1,11 +1,15 @@
-
+/*
+ * A concessionária de veículos “CARANGO VELHO” está vendendo os seus veículos com desconto. 
+ * Faça um algoritmo que calcule e exiba o valor do desconto e o valor a ser pago pelo cliente de vários carros. 
+ * O desconto deverá ser calculado de acordo com o ano do veículo. Até 2000 - 12% e acima de 2000 - 7%. 
+ * O sistema deverá perguntar se deseja continuar calculando desconto até que a resposta seja: “(N) Não”. 
+ * Informar total de carros com ano até 2000 e total geral;
+ */
 package exerciciosintaxe.exerciciosintaxe;
 
 import java.util.Scanner;
 
-
 public class App {
-    
 
     public static void main(String[] args) {
 
@@ -29,26 +33,25 @@ public class App {
 
             System.out.println("Digite o valor do veículo: ");
             valorCarro = leitor.nextFloat();
+
+            if (anoFabricacao <= 2000) {
+                // 12% desconto
+                porcentagemDesconto = 0.12f;
+            } else {
+                porcentagemDesconto = 0.07f;
+                totalCarrosSemiNovos++;
+            }
+            totalCarros++;
+
+            valorDesconto = valorCarro * porcentagemDesconto;
+            valorPagar = valorCarro - porcentagemDesconto;
+
+            System.out.println("Deseja fazer mais cadastros? S - Sim / N - Não");
+            desejaRepetir = leitor.next().charAt(0);
         }
 
-        if (anoFabricacao <= 2000) {
-            //12% desconto
-            porcentagemDesconto = 0.12f;
-        } else {
-            porcentagemDesconto = 0.07f;
-            totalCarrosSemiNovos++;
-        }
-        totalCarros++;
-
-        valorDesconto = valorCarro * porcentagemDesconto;
-        valorPagar = valorCarro - porcentagemDesconto;
-
-        System.out.println("Deseja fazer mais cadastros? S - Sim / N - Não");25
-        desejaRepetir = leitor.next().charAt(0);
-        
         System.out.println("Total de carros semi novos: " + totalCarrosSemiNovos);
         System.out.println("Total de carros: " + totalCarros);
     }
-    
-    
+
 }
